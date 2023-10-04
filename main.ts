141b3d68ee6cb427465e618b304dd1a9d2454a74
cayne-mcdonnell <144451820+cayne-mcdonnell@users.noreply.github.com>
2023-10-04 08:58:17 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -19,11 +19,33 @@ controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
         `, mySprite, 50, 50)
     projectile.follow(mySprite4)
 })
+info.onLifeZero(function () {
+    game.gameOver(false)
+    game.setGameOverMessage(false, "death")
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Food, function (sprite, otherSprite) {
+    sprites.destroy(otherSprite)
+    info.changeLifeBy(1)
+})
+sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Enemy, function (sprite, otherSprite) {
+    sprites.destroy(sprite)
+    enemyhealth += -1
+    if (enemyhealth == 0) {
+        sprites.destroy(mySprite4)
+    }
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
+    info.changeLifeBy(-1)
+    mySprite4.x += 30
+})
 let projectile: Sprite = null
 let mySprite4: Sprite = null
+let enemyhealth = 0
 let mySprite: Sprite = null
+info.setLife(3)
 scene.cameraFollowSprite(mySprite)
 tiles.setCurrentTilemap(tilemap`level1`)
+enemyhealth = 5
 mySprite = sprites.create(img`
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
@@ -104,3 +126,26 @@ mySprite3.setPosition(21, 100)
 mySprite4.setPosition(139, 109)
 mySprite4.follow(mySprite, 50)
 controller.moveSprite(mySprite, 75, 75)
+sprites.destroy(mySprite2)
+game.onUpdateInterval(10000, function () {
+    mySprite3 = sprites.create(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . f f f f f . . . . . 
+        . . . . f f f e e e f f f . . . 
+        . . . f f e e e e e e e f . . . 
+        . . f f e e e e e e e e f f . . 
+        . f f e e e e e e e e e e f . . 
+        . f e e e e e e e e e e e f . . 
+        . f f e e e e e e e e e e f . . 
+        . . f f f f f f f f f f f f . . 
+        . . f f f f d d d d d d d f . . 
+        . f f e e e f f f f f f f f . . 
+        . f f e e e e e e e e e e f f . 
+        . . f f f f e e e e e e e f f . 
+        . . . . . . f f f f f f f f . . 
+        . . . . . . . . . . . . . . . . 
+        `, SpriteKind.Food)
+    mySprite3.x = randint(0, 160)
+    mySprite3.y = randint(0, 120)
+})
